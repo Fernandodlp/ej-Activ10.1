1c5d0f1aa4dd050f6a1f67904a9842f0b8ee254b
Fernandodlp <A01742338@ITESM.MX>
2021-08-27 16:01:57 +0000

    Actividad realizada

--- assignments/00TiendaSillas/src/exercise.py
+++ assignments/00TiendaSillas/src/exercise.py
@@ -1,10 +1,25 @@
 def precio_antes_descuento(tipo_silla, cantidad) :
     # Agrega aquí el código de la funcion
-    pass
+    if tipo_silla == "B" :
+        precio = 700.0 * cantidad
+    elif tipo_silla == "E" :
+        precio = 900.0 * cantidad
+    else :
+        precio = 1500.0 * cantidad
+    return precio
 
 def calcula_descuento(precio, tipo_cl) :
     # Agrega aquí el código de la funcion
-    pass
+    if tipo_cl == "F" :
+        desc = precio * 0.2
+    else :
+        if precio >= 10000 and precio < 20000 :
+            desc = precio * 0.1
+        elif precio >= 20000 :
+            desc = precio * 0.15
+        else :
+            desc = 0.0
+    return desc
 
 def main() :
     # pido el tipo de silla (B E L)
@@ -12,11 +27,10 @@ def main() :
     # pido el tipo de cliente (N F)
     tipo_cl = input("Tipo cliente: ")
     cantidad = int(input("Cantidad de sillas: "))
-
-    subtotal = # Calcula el precio antes del descuento
-    desc = # Calcula del descuento
-    total = # Calcula el total a pagar
-
+ 
+    subtotal = precio_antes_descuento(tipo_silla, cantidad)
+    desc = calcula_descuento(subtotal, tipo_cl)
+    total = subtotal - desc
     print(f"Total sin dcto.  ${subtotal:>7}")
     print(f"Descuento        ${desc:>7}")
     print(f"Total a pagar    ${total:>7}")
